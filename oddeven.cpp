d742647e4c8c63b86c07b7672e7de27792fca72f
Antonyyyyyyy03 <133213143+Antonyyyyyyy03@users.noreply.github.com>
2025-04-07 22:16:41 +0530

    Add files via upload

---
+++ oddeven.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+
+    // Ask the user to enter a number
+    cout << "Enter a number: ";
+    cin >> number;
+
+    // Check if the number is even or odd
+    if (number % 2 == 0) {
+        cout << number << " is even." << endl;
+    } else {
+        cout << number <<
+
